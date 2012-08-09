ec1cfcbc80eb3ed037300380036017af613f00d4
Matt Rardon <mrardon@ncees.org>
2012-08-09 12:42:08 -0400

    Add AWSPricing::EC2 reserved instance pricing

--- lib/aws-pricing/ec2.rb
+++ lib/aws-pricing/ec2.rb
@@ -1,15 +1,42 @@
 module AWSPricing
   class EC2
     #Elastic Compute pricing data
-    
-    EC2_BASE_URL = '/ec2/pricing/pricing-'
-    
-  
+
+    EC2_BASE_URL          = '/ec2/pricing/pricing-'
+    EC2_BASE_RESERVED_URL = '/ec2/pricing/'
+
+
     #Returns Hash of on-demand server instance pricing information
     def self.instances
       Base.get(EC2_BASE_URL + 'on-demand-instances')
     end
-    
+
+    #Returns Hash of reserved linux light utilization instance pricing information
+    def self.reserved_linux_light_instances
+      Base.get(EC2_BASE_RESERVED_URL + '/ri-light-linux')
+    end
+    #Returns Hash of reserved linux medium utilization instance pricing information
+    def self.reserved_linux_medium_instances
+      Base.get(EC2_BASE_RESERVED_URL + '/ri-medium-linux')
+    end
+    #Returns Hash of reserved linux heavy utilization instance pricing information
+    def self.reserved_linux_heavy_instances
+      Base.get(EC2_BASE_RESERVED_URL + '/ri-heavy-linux')
+    end
+
+    #Returns Hash of reserved mswin light utilization instance pricing information
+    def self.reserved_mswin_light_instances
+      Base.get(EC2_BASE_RESERVED_URL + '/ri-light-mswin')
+    end
+    #Returns Hash of reserved mswin medium utilization instance pricing information
+    def self.reserved_mswin_medium_instances
+      Base.get(EC2_BASE_RESERVED_URL + '/ri-medium-mswin')
+    end
+    #Returns Hash of reserved mswin heavy utilization instance pricing information
+    def self.reserved_mswin_heavy_instances
+      Base.get(EC2_BASE_RESERVED_URL + '/ri-heavy-mswin')
+    end
+
     #Returns Hash of current spot instance pricing information (5m)
     def self.spot_instances
       callback_response = Net::HTTP.get_response(URI.parse 'http://spot-price.s3.amazonaws.com/spot.js').body
@@ -17,31 +44,31 @@ module AWSPricing
       callback_response.slice!(callback_response.length-1)
       JSON.parse callback_response
     end
-  
+
     #Returns Hash of elastic block storage pricing information
     def self.ebs
       Base.get(EC2_BASE_URL + 'ebs')
     end
-  
+
     #Returns Hash of data transfer pricing information
     def self.data_transfer
       Base.get(EC2_BASE_URL + 'data-transfer')
     end
-    
+
     #Returns Hash of elastic IP pricing information
     def self.elastic_ips
       Base.get(EC2_BASE_URL + 'elastic-ips')
     end
-    
+
     #Returns Hash of Cloudwatch monitoring pricing information
     def self.cloud_watch
       Base.get(EC2_BASE_URL + 'cloudwatch')
     end
-    
+
     #Returns Hash of elastic loadbalancer pricing information
     def self.elb
       Base.get(EC2_BASE_URL + 'elb')
     end
-      
+
   end
 end
--- spec/aws-pricing_spec.rb
+++ spec/aws-pricing_spec.rb
@@ -3,38 +3,65 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 include AWSPricing
 
 describe "AwsPricing" do
-  
+
   describe "EC2 pricing" do
     it "returns a hash of instance pricing" do
       EC2.instances.class.should == Hash
     end
-  
+
+    describe "reserved instances" do
+      it "returns a hash of reserved linux light utilization instance pricing" do
+        EC2.reserved_linux_light_instances.class.should == Hash
+      end
+
+      it "returns a hash of reserved linux medium utilization instance pricing" do
+        EC2.reserved_linux_medium_instances.class.should == Hash
+      end
+
+      it "returns a hash of reserved linux heavy utlization instance pricing" do
+        EC2.reserved_linux_heavy_instances.class.should == Hash
+      end
+
+
+      it "returns a hash of reserved mswin light utilization instance pricing" do
+        EC2.reserved_mswin_light_instances.class.should == Hash
+      end
+
+      it "returns a hash of reserved mswin medium utilization instance pricing" do
+        EC2.reserved_mswin_medium_instances.class.should == Hash
+      end
+
+      it "returns a hash of reserved mswin heavy utlization instance pricing" do
+        EC2.reserved_mswin_heavy_instances.class.should == Hash
+      end
+    end
+
     it "returns a hash of ebs pricing" do
       EC2.ebs.class.should == Hash
     end
-  
+
     it "returns a hash of data transfer pricing" do
       EC2.data_transfer.class.should == Hash
     end
-    
+
     it "returns a hash of elb pricing" do
       EC2.elb.class.should == Hash
     end
-    
+
     it "returns a hash of cloudwatch pricing" do
       EC2.cloud_watch.class.should == Hash
     end
-    
+
     it "returns a hash of elastic ip pricing" do
       EC2.elastic_ips.class.should == Hash
     end
-    
+
     it "returns a hash of spot instance pricing from jsonp callback hack" do
       EC2.spot_instances.class.should == Hash
     end
-    
+
   end
-  
+
   describe "S3 Pricing" do
     it "returns a hash of storage pricing" do
       S3.storage.class.should == Hash
@@ -46,7 +73,7 @@ describe "AwsPricing" do
       S3.requests.class.should == Hash
     end
   end
-  
+
   describe "Cloudfront pricing" do
     it "returns a hash of requests pricing" do
       CloudFront.data_transfer.class.should == Hash
@@ -55,22 +82,22 @@ describe "AwsPricing" do
       CloudFront.requests.class.should == Hash
     end
   end
-  
+
   describe "Elastic Mapreduce pricing" do
     it "returns a hash of pricing" do
       EMR.pricing.class.should == Hash
     end
   end
-  
+
   describe "RDS pricing" do
     it "returns a hash of standard deployment pricing" do
       RDS.standard.class.should == Hash
     end
-    
+
     it "returns a hash of multi-az deployment pricing" do
       RDS.multi_az.class.should == Hash
     end
-    
+
   end
 end
 

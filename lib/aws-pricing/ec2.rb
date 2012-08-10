ecc9b2e113af66744841868cf52b0822ef05ce44
Matt Rardon <mrardon@ncees.org>
2012-08-10 08:46:34 -0400

    Clean up spaces (trigger travis)

--- lib/aws-pricing/ec2.rb
+++ lib/aws-pricing/ec2.rb
@@ -1,9 +1,9 @@
 module AWSPricing
   class EC2
     #Elastic Compute pricing data
-    
-    
-  
+
+
+
     EC2_BASE_URL          = '/ec2/pricing/pricing-'
     EC2_RESERVED_BASE_URL = '/ec2/pricing/'
 
@@ -21,7 +21,7 @@ module AWSPricing
 
       Base.get(EC2_RESERVED_BASE_URL + "ri-#{opts[:utilization]}-#{opts[:os]}")
     end
-  
+
     #Returns Hash of current spot instance pricing information (5m)
     def self.spot_instances
       callback_response = Net::HTTP.get_response(URI.parse 'http://spot-price.s3.amazonaws.com/spot.js').body
@@ -29,31 +29,31 @@ module AWSPricing
       callback_response.slice!(callback_response.length-1)
       JSON.parse callback_response
     end
-  
+
     #Returns Hash of elastic block storage pricing information
     def self.ebs
       Base.get(EC2_BASE_URL + 'ebs')
     end
-  
+
     #Returns Hash of data transfer pricing information
     def self.data_transfer
       Base.get(EC2_BASE_URL + 'data-transfer')
     end
-    
+
     #Returns Hash of elastic IP pricing information
     def self.elastic_ips
       Base.get(EC2_BASE_URL + 'elastic-ips')
     end
-    
+
     #Returns Hash of Cloudwatch monitoring pricing information
     def self.cloud_watch
       Base.get(EC2_BASE_URL + 'cloudwatch')
     end
-    
+
     #Returns Hash of elastic loadbalancer pricing information
     def self.elb
       Base.get(EC2_BASE_URL + 'elb')
     end
-      
+
   end
 end
